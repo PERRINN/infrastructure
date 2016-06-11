505e50af1b4ceca2d838c3574cf1b65eaa8a3ad0
Chris Moran <chris.moran@emergent.com.au>
2016-06-11 11:52:37 +1000

    fixed AMI variable in script

--- jenkins/packer/base.sh
+++ jenkins/packer/base.sh
@@ -8,9 +8,11 @@ packer build \
 	-var "soe_version=0.1.0" \
 	-var "build_number=1" \
 	-var "build_uuid=${uuid}" \
-	-var "aws_source_ami=ami-75e5cb16" \
+	-var "aws_ami=ami-75e5cb16" \
 	-var "aws_instance_type=t2.micro" \
 	-var "aws_instance_profile=packer-linux" \
+	-var "aws_access_key=ACCESS-KEY" \
+	-var "aws_secret_key=SECRET-KEY" \
 	-var "aws_vpc_id=VPC" \
 	-var "aws_subnet_id=SUBNET" \
 	-var "aws_region_name=${AWS_REGIONNAME}" 
