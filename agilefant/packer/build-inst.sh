940e26af7f667d60edeae29f9f9cb97ea15ae2f9
Chris Moran <Chris Moran>
2016-06-12 14:40:25 +1000

    Adding Agilefant Support

---
+++ agilefant/packer/build-inst.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+sudo rpm -Uvh http://download.newrelic.com/pub/newrelic/el5/i386/newrelic-repo-5-3.noarch.rpm
+sudo yum update -y aws-cfn-bootstrap
+sudo yum erase -y java-1.7.0-openjdk
+sudo yum install -y java-1.8.0-openjdk mysql tomcat8 tomcat8-webapps tomcat8-docs-webapp tomcat8-admin-webapps newrelic-sysmond rsyslog pystache
+
+sudo chkconfig tomcat8 off
+sudo chkconfig newrelic-sysmond off
+sudo chkconfig rsyslog off
