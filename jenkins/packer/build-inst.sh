1a30732787f00d9b9a0e7021ac09a6209b553751
Chris Moran <Chris Moran>
2016-06-12 10:58:43 +1000

    Updated build-inst.sh to make it close to universal.  Now, the only remaining change is to alter it for the maven version.
    Updated jenkins-base.json to upload approved jenkins rpm

--- jenkins/packer/build-inst.sh
+++ jenkins/packer/build-inst.sh
@@ -3,7 +3,8 @@
 sudo yum update -y
 sudo rpm -i /tmp/jdk.rpm
 sudo mkdir /opt/apache
-sudo tar -xf /tmp/apache-maven-3.3.9-bin.tar.gz -C /opt/apache
+sudo tar -xf /tmp/apache-maven.tar.gz -C /opt/apache
 sudo mv /opt/apache/apache-maven-3.3.9 /opt/apache/maven
 sudo yum install -y gcc git
+sudo rpm -i /tmp/jenkins.rpm
 
