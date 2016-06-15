c821b2dd4b20f7f82c73ecc428d5d7dc345e2903
Chris Moran <chris.moran@emergent.com.au>
2016-06-15 16:16:24 +1000

    Added mechanism to report final AMI ID as an artifact in shell script

--- agilefant/packer/agilefant-host-1.sh
+++ agilefant/packer/agilefant-host-1.sh
@@ -6,15 +6,26 @@ AWS_REGIONNAME="ap-southeast-2"
 
 # Generate a unique identified for this build. UUID is the easiest
 uuid=$(date +"%s")
+ver="0.1.0"
+build="1"
+stamp=`date -u +"%Y-%m-%dT%H-%M-%SZ"`
+#echo ${stamp}
 if [ -f ami-id ]
 then
 	rm ami-id
 fi
+if [ -f new_ami ]
+then
+	rm new_ami
+fi
+
+longname=0.1.0-${build}-awsafant-soe-${stamp}
 
 packer build \
-	-var "soe_version=0.1.0" \
-	-var "build_number=1" \
+	-var "soe_version=${ver}" \
+	-var "build_number=${build}" \
 	-var "build_uuid=${uuid}" \
+	-var "build_stamp=${stamp}" \
 	-var "aws_source_ami=ami-75e5cb16" \
 	-var "aws_instance_type=t2.small" \
 	-var "aws_instance_profile=packer-linux" \
@@ -22,3 +33,9 @@ packer build \
 	-var "aws_subnet_id=subnet-8df9b6fa" \
 	-var "aws_region=ap-southeast-2" \
 	agilefant-base.json
+#
+if [ -f ami-id ]
+then
+	aws ec2 describe-images --filters "Name=name,Values=${longname}" | grep ImageId | awk '{ print $2}' | sed -e 's/"//g' | sed -e 's/,//' > new_ami
+fi
+
