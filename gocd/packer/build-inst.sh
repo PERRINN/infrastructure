9352cb47f14f00dedcf4d63a74ce8332465dfe22
Chris Moran <chris.moran@emergent.com.au>
2016-06-17 15:54:51 +1000

    Removing tomcat from Go CD.  Not needed

--- gocd/packer/build-inst.sh
+++ gocd/packer/build-inst.sh
@@ -8,7 +8,6 @@ sudo tar -xf /tmp/apache-maven.tar.gz -C /opt/apache
 sudo mv /opt/apache/apache-maven-3.3.9 /opt/apache/maven
 sudo mv /tmp/gocd.repo /etc/yum.repos.d/gocd.repo
 sudo yum install -y go-server go-server unzip zip gcc git
-sudo rpm -i /tmp/jenkins.rpm
 cd /tmp; unzip packer.zip
 sudo mv /tmp/packer /usr/local/bin/packer
 
