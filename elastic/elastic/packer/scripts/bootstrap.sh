4846664e45e147048b3c305ad9c29dc43be61ca0
Chris Moran <christopher.moran@versent.com.au>
2016-11-24 15:23:42 +1100

    Add elastic X-Pack

--- elastic/elastic/packer/scripts/bootstrap.sh
+++ elastic/elastic/packer/scripts/bootstrap.sh
@@ -9,6 +9,7 @@ sudo yum install -y newrelic-sysmond git java-1.8.0-openjdk
 sudo yum update -y 
 sudo chkconfig newrelic-sysmond off
 sudo wget -O /tmp/elasticsearch.tar.gz https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.0.1.tar.gz
+sudo wget -O/tmp/x-pack.zip https://artifacts.elastic.co/downloads/packs/x-pack/x-pack-5.0.1.zip
 sudo groupadd elastic
 sudo useradd -c "Elastic Search" -d /opt/elasticsearch-5.0.1 -g elastic -s /bin/bash elastic
 sudo tar -xf /tmp/elasticsearch.tar.gz -C /opt
