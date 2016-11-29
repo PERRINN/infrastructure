53bdc039bfd7a2a8b32c1f72ee4ad9658bc79346
Chris Moran <christopher.moran@versent.com.au>
2016-11-30 10:42:30 +1100

    Adding Maven 2 support for Android

--- jenkins/packer/scripts/bootstrap.sh
+++ jenkins/packer/scripts/bootstrap.sh
@@ -5,10 +5,13 @@
 sudo mkdir /opt/apache
 wget -P /tmp http://apache.mirror.serversaustralia.com.au/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 wget -P /tmp https://services.gradle.org/distributions/gradle-3.1-bin.zip
+wget -P /tmp http://archive.apache.org/dist/maven/binaries/apache-maven-2.2.1-bin.tar.gz
 wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/0.10.1/packer_0.10.1_linux_amd64.zip
 wget -O /tmp/ROOT.war http://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war
 sudo tar -xf /tmp/apache-maven-3.3.9-bin.tar.gz -C /opt/apache
 sudo ln -s /opt/apache/apache-maven-3.3.9 /opt/apache/maven
+sudo tar -xf /tmp/apache-maven-2.2.1-bin.tar.gz -C /opt/apache
+sudo ln -s /opt/apache/apache-maven-2.2.1-bin.tar.gz /opt/maven2
 sudo yum erase -y java-1.7.0-openjdk
 sudo rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm
 sudo yum install -y newrelic-sysmond git gcc java-1.8.0-openjdk tomcat8 tomcat8-webapps tomcat8-docs-webapp tomcat8-admin-webapps
@@ -22,6 +25,7 @@ unzip /tmp/gradle-3.1-bin.zip
 sudo mv gradle-3.1 /opt/gradle
 sudo rm /tmp/gradle-3.1-bin.zip
 sudo rm /tmp/apache-maven-3.3.9-bin.tar.gz
+sudo rm /tmp/apache-maven-2.2.1-bin.tar.gz
 sudo service tomcat8 stop
 sudo rm -rf /var/lib/tomcat8/webapps/ROOT
 sudo mv /tmp/ROOT.war /var/lib/tomcat8/webapps
